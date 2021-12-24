4c3317d25341e4054f3242c3d8a59beba6bc7bba
Mikail Oran <mikail.oran@etu.univ-lyon1.fr>
2021-12-24 13:26:26 +0100

    formatting

--- src/tp1.cpp
+++ src/tp1.cpp
@@ -1,19 +1,16 @@
 //! \file tuto9.cpp utilisation d'un shader 'utilisateur' pour afficher un objet Mesh
 
+#include "app_time.h"  // classe Application a deriver
+#include "draw.h"
 #include "mat.h"
 #include "mesh.h"
-#include "wavefront.h"
-
 #include "orbiter.h"
 #include "program.h"
-#include "uniforms.h"
-#include "draw.h"
 #include "texture.h"
+#include "uniforms.h"
+#include "wavefront.h"
 
-#include "app_time.h" // classe Application a deriver
-
-struct Buffers
-{
+struct Buffers {
     GLuint vao;
     GLuint buffer;
 
@@ -23,17 +20,14 @@ struct Buffers
 
     Buffers() : vao(0), buffer(0), vertex_count(0) {}
 
-    void create(Mesh &mesh)
-    {
+    void create(Mesh &mesh) {
         if (!mesh.vertex_buffer_size())
             return;
 
         auto groups = mesh.groups();
         std::vector<int> material_idx;
-        for (int i = 0; i < int(groups.size()); i++)
-        {
-            for (int j = 0; j < groups[i].n; ++j)
-            {
+        for (int i = 0; i < int(groups.size()); i++) {
+            for (int j = 0; j < groups[i].n; ++j) {
                 material_idx.push_back(groups[i].index);
             }
         }
@@ -52,10 +46,10 @@ struct Buffers
         size = mesh.vertex_buffer_size();
         glBufferSubData(GL_ARRAY_BUFFER, offset, size, mesh.vertex_buffer());
         glVertexAttribPointer(0,
-                              3, GL_FLOAT, // size et type, position est un vec3 dans le vertex shader
-                              GL_FALSE,    // pas de normalisation des valeurs
-                              0,           // stride 0, les valeurs sont les unes a la suite des autres
-                              0            // offset 0, les valeurs sont au debut du buffer
+                              3, GL_FLOAT,  // size et type, position est un vec3 dans le vertex shader
+                              GL_FALSE,     // pas de normalisation des valeurs
+                              0,            // stride 0, les valeurs sont les unes a la suite des autres
+                              0             // offset 0, les valeurs sont au debut du buffer
         );
         glEnableVertexAttribArray(0);
 
@@ -71,19 +65,19 @@ struct Buffers
         size = mesh.normal_buffer_size();
         glBufferSubData(GL_ARRAY_BUFFER, offset, size, mesh.normal_buffer());
         glVertexAttribPointer(2,
-                              3, GL_FLOAT, // size et type, normal est un vec3 dans le vertex shader
-                              GL_FALSE,    // pas de normalisation des valeurs
-                              0,           // stride 0, les valeurs sont les unes a la suite des autres
+                              3, GL_FLOAT,  // size et type, normal est un vec3 dans le vertex shader
+                              GL_FALSE,     // pas de normalisation des valeurs
+                              0,            // stride 0, les valeurs sont les unes a la suite des autres
                               (const GLvoid *)offset);
         glEnableVertexAttribArray(2);
 
         // attribut 4 material
         offset += size;
-        size = material_idx.size() * sizeof(int);
+        size = material_idx.size() * sizeof(unsigned int);
         glBufferSubData(GL_ARRAY_BUFFER, offset, size, &material_idx.front());
         glVertexAttribIPointer(4,
-                               1, GL_UNSIGNED_INT, // size et type, normal est un vec3 dans le vertex shader
-                               0,                  // stride 0, les valeurs sont les unes a la suite des autres
+                               1, GL_UNSIGNED_INT,  // size et type, normal est un vec3 dans le vertex shader
+                               0,                   // stride 0, les valeurs sont les unes a la suite des autres
                                (const GLvoid *)offset);
         glEnableVertexAttribArray(4);
 
@@ -91,15 +85,13 @@ struct Buffers
         vertex_count = mesh.vertex_count();
     }
 
-    void release()
-    {
+    void release() {
         glDeleteBuffers(1, &buffer);
         glDeleteVertexArrays(1, &vao);
     }
 };
 
-GLuint make_texture_array(const int unit, const std::vector<ImageData> &images, const GLenum texel_format = GL_RGBA)
-{
+GLuint make_texture_array(const int unit, const std::vector<ImageData> &images, const GLenum texel_format = GL_RGBA) {
     assert(images.size());
 
     // verifie que toutes les images sont au meme format
@@ -109,15 +101,14 @@ GLuint make_texture_array(const int unit, const std::vector<ImageData> &images, 
     int s = images[0].size;
     int d = int(images.size());
 
-    for (unsigned i = 1; i < images.size(); i++)
-    {
+    for (unsigned i = 1; i < images.size(); i++) {
         if (images[i].pixels.size() == 0)
-            continue; // pas de pixels, image pas chargee ?
+            continue;  // pas de pixels, image pas chargee ?
 
         if (images[i].width != w)
-            return 0; //  pas la meme largeur
+            return 0;  //  pas la meme largeur
         if (images[i].height != h)
-            return 0; // pas la meme hauteur
+            return 0;  // pas la meme hauteur
     }
 
     // alloue le tableau de textures
@@ -131,42 +122,39 @@ GLuint make_texture_array(const int unit, const std::vector<ImageData> &images, 
                  GL_RGBA, GL_UNSIGNED_BYTE, nullptr);
 
     // transfere les textures
-    for (unsigned i = 0; i < images.size(); i++)
-    {
+    for (unsigned i = 0; i < images.size(); i++) {
         if (images[i].pixels.size() == 0)
             continue;
 
         // recupere les parametres de conversion...
         GLenum format;
-        switch (images[i].channels)
-        {
-        case 1:
-            format = GL_RED;
-            break;
-        case 2:
-            format = GL_RG;
-            break;
-        case 3:
-            format = GL_RGB;
-            break;
-        case 4:
-            format = GL_RGBA;
-            break;
-        default:
-            format = GL_RGBA;
+        switch (images[i].channels) {
+            case 1:
+                format = GL_RED;
+                break;
+            case 2:
+                format = GL_RG;
+                break;
+            case 3:
+                format = GL_RGB;
+                break;
+            case 4:
+                format = GL_RGBA;
+                break;
+            default:
+                format = GL_RGBA;
         }
 
         GLenum type;
-        switch (images[i].size)
-        {
-        case 1:
-            type = GL_UNSIGNED_BYTE;
-            break;
-        case 4:
-            type = GL_FLOAT;
-            break;
-        default:
-            type = GL_UNSIGNED_BYTE;
+        switch (images[i].size) {
+            case 1:
+                type = GL_UNSIGNED_BYTE;
+                break;
+            case 4:
+                type = GL_FLOAT;
+                break;
+            default:
+                type = GL_UNSIGNED_BYTE;
         }
 
         glTexSubImage3D(GL_TEXTURE_2D_ARRAY, /* mipmap */ 0,
@@ -192,21 +180,19 @@ GLuint make_texture_array(const int unit, const std::vector<ImageData> &images, 
     return texture;
 }
 
-class TP : public AppTime
-{
-public:
+class TP : public AppTime {
+   public:
     // constructeur : donner les dimensions de l'image, et eventuellement la version d'openGL.
     TP() : AppTime(1024, 640) {}
 
-    int init()
-    {
+    int init() {
         // Mesh mesh= read_mesh("data/cube.obj");
         // Mesh mesh= read_mesh("data/robot.obj");
         // Mesh mesh= read_mesh("data/assets/roads_quaternion/OBJ/Street_3Way_2.obj");
         // Mesh mesh= read_mesh("data/assets/roads_quaternion/OBJ/Street_Bridgeroads_quaternion/OBJ/Street_Bridge_Ramp.obj");
         Mesh mesh = read_mesh("data/assets/bistro-small/exterior.obj");
         if (mesh.materials().count() == 0)
-            return -1; // pas de matieres, pas d'affichage
+            return -1;  // pas de matieres, pas d'affichage
 
         m_objet.create(mesh);
 
@@ -224,15 +210,13 @@ public:
         const Materials &materials = mesh.materials();
         assert(materials.count() <= int(m_colors.size()));
 
-        for (int i = 0; i < materials.count(); i++)
-        {
+        for (int i = 0; i < materials.count(); i++) {
             m_colors[i] = materials.material(i).diffuse;
             m_textures[i] = materials.material(i).diffuse_texture;
         }
 
         std::vector<ImageData> images;
-        for (int i = 0; i < materials.filename_count(); i++)
-        {
+        for (int i = 0; i < materials.filename_count(); i++) {
             ImageData image = read_image_data(materials.filename(i));
             images.emplace_back(image);
         }
@@ -250,18 +234,17 @@ public:
         // }
 
         // etat openGL par defaut
-        glClearColor(0.2f, 0.2f, 0.2f, 1.f); // couleur par defaut de la fenetre
+        glClearColor(0.2f, 0.2f, 0.2f, 1.f);  // couleur par defaut de la fenetre
 
-        glClearDepth(1.f);       // profondeur par defaut
-        glDepthFunc(GL_LESS);    // ztest, conserver l'intersection la plus proche de la camera
-        glEnable(GL_DEPTH_TEST); // activer le ztest
+        glClearDepth(1.f);        // profondeur par defaut
+        glDepthFunc(GL_LESS);     // ztest, conserver l'intersection la plus proche de la camera
+        glEnable(GL_DEPTH_TEST);  // activer le ztest
 
-        return 0; // ras, pas d'erreur
+        return 0;  // ras, pas d'erreur
     }
 
     // destruction des objets de l'application
-    int quit()
-    {
+    int quit() {
         // etape 3 : detruire le shader program
         release_program(m_program);
         m_objet.release();
@@ -269,11 +252,8 @@ public:
     }
 
     // dessiner une nouvelle image
-    int render()
-    {
-
-        if (key_state('r'))
-        {
+    int render() {
+        if (key_state('r')) {
             clear_key_state('r');
             reload_program(m_program, "src/shader/tp1_color.glsl");
         }
@@ -282,11 +262,11 @@ public:
         // deplace la camera
         int mx, my;
         unsigned int mb = SDL_GetRelativeMouseState(&mx, &my);
-        if (mb & SDL_BUTTON(1)) // le bouton gauche est enfonce
+        if (mb & SDL_BUTTON(1))  // le bouton gauche est enfonce
             m_camera.rotation(mx, my);
-        else if (mb & SDL_BUTTON(3)) // le bouton droit est enfonce
+        else if (mb & SDL_BUTTON(3))  // le bouton droit est enfonce
             m_camera.move(mx);
-        else if (mb & SDL_BUTTON(2)) // le bouton du milieu est enfonce
+        else if (mb & SDL_BUTTON(2))  // le bouton du milieu est enfonce
             m_camera.translation((float)mx / (float)window_width(), (float)my / (float)window_height());
 
         glUseProgram(m_program);
@@ -327,7 +307,7 @@ public:
         return 1;
     }
 
-protected:
+   protected:
     Transform m_model;
     Buffers m_objet;
     Orbiter m_camera;
@@ -338,8 +318,7 @@ protected:
     std::vector<int> m_textures;
 };
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     TP tp;
     tp.run();
 
